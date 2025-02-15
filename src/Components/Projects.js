eccc3a26647556562b0642e55870f287fa957199
DIVAKAR A <divakargiri1718@gmail.com>
2025-02-15 09:44:29 +0530

    changed the css of projectbox

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
-import NewsletterImage from '../images/NewsletterImage.png';
-import RogfreeImage from '../images/RogfreeImage.png';
-import TindogImage from '../images/TindogImage.png';
-import WigglesImage from '../images/WigglesImage.png';
+import TripPlannerImage from '../images/tripplanner.png';
+import ECommerceImage from '../images/e-commerce.png';
+import DoctorAppImage from '../images/doctor.png';
 
 const Projects = () => {
   return (
     <div>
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={WigglesImage} projectName="Wiggles" />
-        <ProjectBox projectPhoto={NewsletterImage} projectName="Newsletter" />
-        <ProjectBox projectPhoto={RogfreeImage} projectName="RogFree" />
-        <ProjectBox projectPhoto={TindogImage} projectName="Tindog" />
+        <ProjectBox projectPhoto={TripPlannerImage} projectName="TripPlanner" />
+        <ProjectBox projectPhoto={ECommerceImage} projectName="ECommerce" />
+        <ProjectBox projectPhoto={DoctorAppImage} projectName="DoctorApp" />
       </div>
-
     </div>
-  )
+  );
 }
-
-export default Projects
+export default Projects;
